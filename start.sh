07f44a437bab440793d9ab040c538afd37a6e50d
Pankajpandiyar <108175184+Pankajpandiyar@users.noreply.github.com>
2023-01-05 23:42:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://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@github.com/Pankajpandiyar/LUCIFER-NEW.git /Lucifer-NEW
+  git clone https://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@github.com/Pankajpandiyar/LUCIFER-NEW.git /LUCIFER-NEW
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Lucifer-NEW
+  git clone $UPSTREAM_REPO /LUCIFER-NEW
 fi
 cd /LUCIFER-NEW
 pip3 install -U -r requirements.txt
