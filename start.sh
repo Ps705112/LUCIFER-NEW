ee825d8f89ec2dfa6eb3f9635a2cfd12178562af
Pankajpandiyar <108175184+Pankajpandiyar@users.noreply.github.com>
2023-01-05 23:40:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Pankajpandiyar/LUCIFER-NEW.git /Lucifer-NEW
+  git clone https://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@github.com/Pankajpandiyar/LUCIFER-NEW.git /Lucifer-NEW
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Lucifer-NEW
